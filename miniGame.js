bc8d21e2f43bbe6f608074881e597d374fe8cf94
Andrei Gaidai <innwindu@gmail.com>
2024-12-05 19:19:00 +0300

    Without array

--- miniGame.js
+++ miniGame.js
@@ -1,25 +1,21 @@
-const numbers = [];
-for (let i = 1; i <= 100; i++) {
-  numbers.push(i);
-}
-
 const guessSecretNumber = () => {
-  const secretNumber = Math.floor(Math.random() * (100 - 1) + 1);
-  let start = 0;
-  let end = numbers.length - 1;
+  let start = 1;
+  let end = 100;
   let found = false;
   let middle;
+  const secretNumber = Math.floor(Math.random() * (end - start) + 1);
+  console.log('Компьютер 1 загадал число: ' + secretNumber);
 
   while (start <= end && found === false) {
     middle = Math.floor((start + end) / 2);
-    console.log('Компьютер 2: Пробую число ' + numbers[middle] + '.');
-    if (secretNumber === numbers[middle]) {
+    console.log('Компьютер 2: Пробую число ' + middle + '.');
+    if (secretNumber === middle) {
       found = true;
-      console.log('Компьютер 1: Угадал.');
-    } else if (secretNumber < numbers[middle]) {
+      console.log('Компьютер 1: Угадал!');
+    } else if (secretNumber < middle) {
       console.log('Компьютер 1: Меньше.');
       end = middle - 1;
-    } else if (secretNumber > numbers[middle]) {
+    } else if (secretNumber > middle) {
       console.log('Компьютер 1: Больше.');
       start = middle + 1;
     }
